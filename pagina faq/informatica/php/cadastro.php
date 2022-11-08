ae0d83b012cd3f56864f7d3b427b6aa82cb406e1
Wesnnen Silva <wesnnen@gmail.com>
2022-11-08 09:49:55 -0300

    Estudos de Front-end
    estudos

---
+++ pagina faq/informatica/php/cadastro.php
@@ -0,0 +1,22 @@
+
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="..\css\style.css">
+    <title>Informatica</title>
+</head>
+<body>
+<nav class="cent">
+<div class="respostas">
+<p style="color: white;">
+Bem Vindo <?php echo $_POST["nome"]; ?><br>
+O seu e-mail : <?php echo $_POST["e-mail"]; ?><br>
+O seu CIM : <?php echo $_POST["cim"]; ?>
+</p>
+</div>
+</nav>
+</body>
+</html>
